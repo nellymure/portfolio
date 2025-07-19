857029ad70349af33953838765ea95950674ae8c
romuald-rugoletto <rugoletto.romuald@gmail.com>
2025-07-19 14:59:03 +0200

    move GradientsBg to HomeView
    * improve css for responsive

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,16 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <!-- <HeaderOutline class="test">
+      <template v-slot:content>
+        <h1>une suite de mots sans signification utilisye à titre provisoire</h1>
+      </template>
+    </HeaderOutline>
+    <HeaderOutline class="test">
+      <template v-slot:content>
+        <h6>une suite de mots sans signification utilisye à titre provisoire</h6>
+      </template>
+    </HeaderOutline> -->
 </template>
 
 <style>
