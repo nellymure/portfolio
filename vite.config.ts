393bbd4def576113dafb23ed0d08496616de1ef1
romuald-rugoletto <rugoletto.romuald@gmail.com>
2025-09-01 19:34:18 +0200

    add vite legacy

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-
-/**
- * Minimal versions for Copilot:
- * Chrome : 50+
- * Firefox : 48+
- * Safari : 13.1+
- * Edge : 79+
- * iOS Safari : 13.4+
- * Android Chrome/WebView : 61+
- */
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [vue(), vueDevTools(), vueI18nPlugin()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    vueI18nPlugin(),
+    legacy({
+      targets: ['chrome >= 50', 'edge >= 79', 'safari >= 13.1', 'firefox >= 48'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      modernPolyfills: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
